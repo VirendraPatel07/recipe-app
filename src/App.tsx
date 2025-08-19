cdaed96e670b75938e459f9685155d68cf456506
virendra-patel-07 <virendra.patel@futurealgos.com>
2025-08-19 12:17:27 +0530

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(){
     const navigation = useNavigation();
     // The useNavigation hook provides information about the current navigation state.
     // It can be used to determine if the app is currently navigating, which can be useful for showing loading indicators or disabling UI elements during transitions.
-    const isLoading = navigation.state === 'loading';
+    //const isLoading = navigation.state === 'loading';
     //const isNavigating = navigation.state === 'navigating';
     // The isLoading and isNavigating variables can be used to conditionally render loading indicators or disable UI elements during navigation.
     const isNavigating = Boolean(navigation.location);
